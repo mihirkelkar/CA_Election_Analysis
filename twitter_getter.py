2adb2c3632ef9607affad4a3b8c2cfd83eb2d790
mihirkelkar <kelkarmhr@gmail.com>
2014-09-23 17:59:44 -0400

    Committing Data gathered for #17
    Committing Data gathered for #17

---
+++ twitter_getter.py
@@ -0,0 +1,32 @@
+import time
+import sys
+import json
+from TwitterSearch import *
+
+OAuth = TwitterSearch(
+      consumer_key = 'EFBFU5OyPJ8eogw98nubFYqrv',
+      consumer_secret = 'LH8NdCZcGi5fgWe9tWrbTdbnB5EdFs05XV03fG1x8guleeu0S5',
+      access_token = '2828646955-lGyZgUyrBoeoWguK9BzumA5tYKHmbchm9IOIcXB',
+      access_token_secret = '15OrYXdvW1BcaQZYHSqFfKcn37uDuSYwlOig5T7VwgHWr'
+    )
+
+for ii in range(1, 100):
+  try:
+    congress_tweets = TwitterSearchOrder()
+    congress_tweets.setKeywords(['#ca17', '#CA17'])
+    congress_tweets.setLanguage('en')
+    congress_tweets.setCount(100)
+    congress_tweets.setIncludeEntities(False)
+    if ii > 1:
+      congress_tweets.setSinceID(since_id)
+    json_list = list() 
+    for tweet in OAuth.searchTweetsIterable(congress_tweets):
+      json_list.append(tweet)
+      json_list.append(tweet)
+    since_id = int(json_list[-1]['id'])
+    with open("tweets_data/test/%s.json" %("hashtag_CA17_" + str(ii)), "w") as output:
+      json.dump(json_list, output)
+  except TwitterSearchException as e:
+    print e
+    print "Something went wrong"
+  time.sleep(2)
